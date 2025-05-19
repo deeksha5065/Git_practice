111b4589083215b2981ca6bf7605e318dd092314
vakalapudi-deekshitha <20p31a04n8@acet.ac.in>
2025-05-19 19:50:56 +0530

    p1 file deleted

--- p1.py
+++
@@ -1,9 +0,0 @@
-Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> abc
-Traceback (most recent call last):
-  File "<pyshell#0>", line 1, in <module>
-    abc
-NameError: name 'abc' is not defined. Did you mean: 'abs'? Or did you forget to import 'abc'?
->>> print("suma")
-suma
